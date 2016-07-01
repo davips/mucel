deaec127ea1a64b966f947c451e2bb2d03dbd31b
davips <you@example.com>
2016-06-30 23:58:05 -0300

    funfando, cinetica ok; falta respeitar qtd de mov de acordo com area e falta corpo rigido

--- src/Bio.hs
+++ src/Bio.hs
@@ -1,46 +1,98 @@
 module Bio(World(World), particleInfo, initialWorld, evolve, organisms) where
-import Geometry
+import Geometry; import Data.Maybe; import Debug.Trace; import Data.Function (on)
 import qualified Data.PQueue.Prio.Min as H
-import Data.Maybe
--- import qualified Data.Set as S
-import Data.Function (on)
+import qualified Data.Vector as Ve
 
+type Horg = H.MinPQueue HeapNode Float
+type Orgs = Ve.Vector Organism
+data World = World {organisms::Orgs, collisionHeap::Horg}
+data HeapNode = Node {fstOrgId::Int, sndOrgId::Int, timeHeapNode::Float} deriving (Show, Eq)
+instance Ord HeapNode where compare = compare `on` timeHeapNode
 data Organism = Uni {particleInfo::ParticleInfo}
-              | Multi {particleInfo::ParticleInfo, aInfo::AngularInfo, subOrgs::[Organism]} deriving (Show)
+              | Multi {particleInfo::ParticleInfo, aInfo::AngularInfo, subOrgs::[Organism]}
+              | Wall {particleInfo::ParticleInfo} deriving (Show)
 
-instance Eq Organism where Uni particleInfoa == Uni particleInfob = particleInfoa == particleInfob
-instance Ord Organism where compare = compare `on` particleInfo
-data World = World {organisms::[Organism], orgHeap::Horg}
-type Horg = H.MinPQueue Float (Organism, Organism)
--- type Sorg = S.Set Organism
+debug res = traceShow res res
+orgId = particleId . particleInfo
+orgPos = particlePos . particleInfo
+orgVel = particleVel . particleInfo
+updatePV org p v = org {particleInfo = (particleInfo org) {particlePos = p, particleVel = v}}
+distOrgs a b = dist (orgPos a) (orgPos b)
+initialWorld = World orgs heap
+  where list = [Wall $ ParticleInfo 0 (V (1000) (1000)) (V 0 0) 0
+              , Wall $ ParticleInfo 1 (V (-1000) (1000)) (V 0 0) 0
+              , Wall $ ParticleInfo 2 (V (-1000) (-1000)) (V 0 0) 0
+              , Wall $ ParticleInfo 3 (V (1000) (-1000)) (V 0 0) 0
+              , uni 4   (100)  (-100)   1000 (-165)  12
+              , uni 5    (-100)  0      0 3000    60
+              , uni 6     100  0      (-112) 300    10
+              , uni 7   (-100) (-100)   (-2100)  50    15
+              , uni 8   (-100)   100    10  800    20
+              , uni 9   (100)    100    (-90)  300    16
+              , uni 10   (900)  (-100)   100 (-605)  12
+              , uni 11    (-800)  0      0 0    60
+              , uni 12     700  0      (-212) 2000    10
+              , uni 13   (-500) (-100)   10  50    15
+              , uni 14   (-400)   100    10  800    20
+              , uni 15   (300)    100    (-900)  30    16
+              , uni 16   (200)  (-100)   100 (-605)  12
+              , uni 17    (-100)  0      0 0    60
+              , uni 18     0  0      (-1120) 0    10
+              , uni 19   (-100) (-900)   1000  500    15
+              , uni 20   (-100)   800    10  8000    20
+              , uni 21   (100)    700    (-9000)  30    16
+              ]
+        heap = H.fromList nodes
+        nodes = [(Node (orgId a) (orgId b) (timeToCollision a b), 0) | a <- list, b <- list, orgId a < orgId b]
+        orgs = Ve.fromList list
 
 uni :: Int -> Float -> Float -> Float -> Float -> Float -> Organism
 uni ido xpos ypos xvel yvel rad = Uni $ ParticleInfo ido (V xpos ypos) (V xvel yvel) rad
 
-initialWorld = World orgs heap
-  where orgs = [uni 1 100 0 (-10) 0 40, uni 1 0 100 10 0 50]
-        heap = H.fromList pairs
-        pairs = [(timeToHitOrgs a b, (a, b)) | a <- orgs, b <- orgs]
-
-timeToHitOrgs a b = fromMaybe 7200000000 $ timeToHit (particleInfo a) (particleInfo b)
-
-orgPos = particlePos . particleInfo
-distOrgs a b = dist (orgPos a) (orgPos b)
+timeToCollision :: Organism -> Organism -> Float
+timeToCollision (Wall particleInfoW) b = fromMaybe veryLargeFloat $ timeToHitWall (particleInfo b) particleInfoW
+timeToCollision a b@(Wall _) = timeToCollision b a
+timeToCollision a b = fromMaybe veryLargeFloat $ timeToHit (particleInfo a) (particleInfo b)
 
 evolve :: World -> Float -> World
 evolve world 0 = world
-evolve (World orgs heap) timeStep = evolve newWorld remainingTime
-  where newWorld = World (map (move timeToFirstCollision) orgs) newHeap
-        remainingTime = timeStep - timeToFirstCollision
-        (timeToFirstCollision, _) = H.findMin heap
-        newHeap = heap
+evolve (World orgs heap) timeStep = evolve newWorld remainingTime -- This recursion allows more fidelity in the animation.
+  where newWorld = World newOrgs newHeap
+        remainingTime = timeStep - dt
+        newOrgs = Ve.map (updateOrg timeToFirstCollision dt orgIdA orgIdB posA posB velA velB) orgs
+        newHeap = if timeStep <= timeToFirstCollision then H.mapKeysMonotonic (\x -> x {timeHeapNode = timeHeapNode x - dt}) heap
+                  else H.mapKeys (updateNode timeToFirstCollision newOrgs dt orgIdA orgIdB) heap
+        dt = if timeToFirstCollision==0 then timeStep else min timeToFirstCollision timeStep
+        (Node orgIdA orgIdB timeToFirstCollision, _) = H.findMin heap
+        posA = orgPos $ moveUntil dt $ Ve.unsafeIndex orgs orgIdA
+        posB = orgPos $ moveUntil dt $ Ve.unsafeIndex orgs orgIdB
+        velA = orgVel $ Ve.unsafeIndex orgs orgIdA
+        velB = orgVel $ Ve.unsafeIndex orgs orgIdB
 
-move :: Float -> Organism -> Organism
-move dt u@(Uni p@(ParticleInfo oid oldPos vel _)) = u {particleInfo = p {particlePos = oldPos `add` (dt `sca` vel)}}
-move dt m@(Multi p@(ParticleInfo oid oldPos vel _) _ _) = m {particleInfo = p {particlePos = oldPos `add` (dt `sca` vel)}}
+updateOrg _ 0 _ _ _ _ _ _ org = org -- fazer batida nesse caso?
+updateOrg _ _ _ _ _ _ _ _ w@(Wall _) = w
+updateOrg timeToFirstCollision dt idA idB posA posB velA velB org
+  | timeToFirstCollision == dt && orgId org == idB && (idA == 0 || idA == 2) = updatePV org posB velB {vecY = -vecY velB}
+  | timeToFirstCollision == dt && orgId org == idB && (idA == 1 || idA == 3) = updatePV org posB velB {vecX = -vecX velB}
+  | timeToFirstCollision == dt && orgId org == idA = updatePV org posA newVelA
+  | timeToFirstCollision == dt && orgId org == idB = updatePV org posB newVelB
+  | otherwise                                      = moveUntil dt org --existeessa cond?
+    where newVelA = traceShow "bateu" $ ficaNoA `add` vaiProA
+          newVelB = ficaNoB `add` vaiProB
+          (ficaNoA, vaiProB) = decompoe posA posB velA
+          (ficaNoB, vaiProA) = decompoe posB posA velB
 
+updateNode t1 _ t2 _ _ pair | t2 == 0 = pair
+updateNode timeToFirstCollision orgs dt idAhit idBhit pair@(Node idA idB time) = pair {timeHeapNode = newTime}
+  where newTime = if idA == idAhit || idA == idBhit || idB == idAhit || idB == idBhit
+                  then timeToCollision (orgs `Ve.unsafeIndex` idA) (orgs `Ve.unsafeIndex` idB)
+                  else time - dt
 
-
+moveUntil :: Float -> Organism -> Organism
+moveUntil 0 x = x
+moveUntil dt u@(Uni p@(ParticleInfo oid oldPos vel _)) = u {particleInfo = p {particlePos = oldPos `add` (dt `sca` vel)}}
+moveUntil dt m@(Multi p@(ParticleInfo oid oldPos vel _) _ _) = m {particleInfo = p {particlePos = oldPos `add` (dt `sca` vel)}}
+moveUntil _ x = x
 
 
 
@@ -94,42 +146,14 @@ move dt m@(Multi p@(ParticleInfo oid oldPos vel _) _ _) = m {particleInfo = p {p
 -- idAliza::Cell -> (Int, Cell)
 -- idAliza ce = (cellId ce, ce)
 --
--- hitWall cella@(Cell _ pi@(V x y) v@(V vx vy) _ _)
---      | xf < -w + r || xf > w - r = cella {vel = V (-vx) vy}
---      | yf < -h + r || yf > h  - r = cella {vel = V vx (-vy)}
---      | otherwise = cella
---      where V xf yf = add pi v
 --
--- speedTransfer cella@(Cell _ pa va _ _) cellb@(Cell _ pb _ _ _) = (cella {vel = fica}, cellb {vel = vai})
---   where (fica, vai) = decompoe pa pb va
---
--- firstColl :: Cell -> [Cell] -> Maybe Cell
--- firstColl (Cell _ pa va _ _) cells = if length res == 0 then Nothing
---                                            else Just (minimumBy (comparing $ \cellb -> dist pa (pos cellb)) res)
---      where res = risky pa va cells
---
--- risky p v cells = filter ((isNear p v) . pos) cells
---
---
--- world = world'
---      ++ map (\x@(Cell id p v _ _) -> x {cellId = id + length world', pos = sca 1.2 p}) world'
---                ++ map (\x@(Cell id p v _ _) -> x {cellId = id + 2 * length world', pos = sca 0.3 p}) world'
---                ++ map (\x@(Cell id p v _ _) -> x {cellId = id + 3 * length world', pos = sca 0.4 p}) world'
---                ++ map (\x@(Cell id p v _ _) -> x {cellId = id + 4 * length world', pos = sca 0.5 p}) world'
---
--- r2' = r2 * 3
--- world' = [
---            Cell 0 (V 0 0) (V 0 0) None (Motor $ V (-1) (-1))
---           , Cell 1 (V 0 (-r2')) (V (-0.07) 0.05) None Battery
---           , Cell 2 (V (-r2') (-r2')) (V 0.02 0) None Wire
---           , Cell 3 (V (-r2') 0) (V (0.01) (-0.01)) Bulb Battery
---           , Cell 6 (V (r2') (-r2')) (V mm 0) Sensor (Motor $ V 1 10)
---           , Cell 4 (V (-r2') r2') (V mm 0) None (Motor (V 10 10))
---           , Cell 5 (V (0) (r2')) (V 0 0) Bulb Wire
---           , Cell 7 (V (r2') (r2')) (V (-mm) (mm)) Bulb (Motor $ V 10 1)
---      ]
 --
 -- randomlist :: Int -> StdGen -> [Int]
 -- randomlist n = take n . unfoldr (Just . random)
 --
 -- nei = map (flip mod 2) $ randomlist 6 seed
+
+
+
+--instance Eq Organism where Uni particleInfoa == Uni particleInfob = particleInfoa == particleInfob
+--instance Ord Organism where compare = compare `on` particleInfo
--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,6 @@
 module Main(main) where
 import Bio
 import View
--- import Debug.Trace
--- import Data.Maybe
--- import Data.List
--- import Graphics.Gloss
--- import Graphics.Gloss.Interface.Pure.Simulate (simulate,ViewPort)
---
--- window :: (ViewPort -> Float -> World -> World) -> [Cell] -> IO ()
--- window step cells = simulate g black 500 (World cells) draw step
---   where g = InWindow "bla" (round w, round h) (0, 0) --FullScreen (1920, 1080)
---
--- draw :: World -> Picture
--- draw (World cells) = pictures $ (color white $ Line [(-w,-h), (-w,h), (w,h), (w,-h), (-w,-h)]) : map drawCell cells
---
--- update :: ViewPort -> Float -> World -> World
--- -- update _ dt (World bolas) = World $ map (move . electrify . luminify) bolas
--- --update _ dt (World cells) = World $ moveAll cells
--- update _ dt (World cells) = World $ traceShow (sum $ map ((^2).mag.vel) cells) $ moveAll cells
---
--- main :: IO ()
 
 main :: IO ()
 main = window update initialWorld
